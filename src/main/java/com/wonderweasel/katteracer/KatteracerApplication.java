62c8c614830d15e7cc724644b4fd305cfa162317
WonderWeasel01 <alexwentzel@live.dk>
2023-04-17 10:09:47 +0200

    SPRING

---
+++ src/main/java/com/wonderweasel/katteracer/KatteracerApplication.java
@@ -0,0 +1,13 @@
+package com.wonderweasel.katteracer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatteracerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatteracerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wonderweasel/katteracer/KatteracerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonderweasel.katteracer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatteracerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
