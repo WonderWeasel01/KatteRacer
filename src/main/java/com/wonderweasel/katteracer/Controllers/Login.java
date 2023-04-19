eb7ac5134e30333ed07b14bf9d5bdda396077105
WonderWeasel01 <alexwentzel@live.dk>
2023-04-19 05:40:04 -0700

    test

---
+++ src/main/java/com/wonderweasel/katteracer/Controllers/Login.java
@@ -0,0 +1,13 @@
+package com.wonderweasel.katteracer.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+
+public class Login {
+
+
+
+}
