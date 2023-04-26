a623975057f1eeae5e1bec195591c476210bf69a
Justinxc1337 <110351655+Justinxc1337@users.noreply.github.com>
2023-04-26 15:23:16 +0200

    java code

--- src/main/java/com/wonderweasel/katteracer/KatteracerApplication.java
+++ src/main/java/com/wonderweasel/katteracer/KatteracerApplication.java
@@ -2,12 +2,57 @@ package com.wonderweasel.katteracer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
+@RestController
+@Controller
 public class KatteracerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KatteracerApplication.class, args);
 	}
 
+	@GetMapping("/forside")
+    @ResponseBody
+    public String index() throws IOException {
+        ResourceLoader resourceLoader = new DefaultResourceLoader();
+        Resource resource = resourceLoader.getResource("classpath:/static/forside.html");
+        InputStream inputStream = resource.getInputStream();
+        byte[] bytes = StreamUtils.copyToByteArray(inputStream);
+        String htmlContent = new String(bytes, StandardCharsets.UTF_8);
+        return htmlContent;
+
+    }
+
+	@GetMapping("/hej")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    /* up and down both does nothing other than saying their own shit currently */
+    @GetMapping("/medarbejder")
+    public String medarbejder() {
+        return "medarbejder";
+    }
+
+    @GetMapping("/?username=dennis&password=umaru")
+    public String xx() {
+        return "unknown statement required, file is [REDACTED]";
+    }
+
 }
+
+
+   
+
+    
